6467f2e84b0cd7c09b86975709132b4d54ef404c
itzJovens <82013409+itzJovens@users.noreply.github.com>
2021-07-30 21:52:54 -0400

    Update app.js

--- app.js
+++ app.js
@@ -105,3 +105,22 @@ function syncDelay(milliseconds){
 function onMessageHandler(target, context, msg, self) {
     if (self) { return; } // Ignore messages from the bot
 }
+
+String.prototype.toHHMMSS = function () {
+    var sec_num = parseInt(this, 10); // don't forget the second param
+    var hours   = Math.floor(sec_num / 3600);
+    var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
+    var seconds = sec_num - (hours * 3600) - (minutes * 60);
+
+    if (hours   < 10) {hours   = "0"+hours;}
+    if (minutes < 10) {minutes = "0"+minutes;}
+    if (seconds < 10) {seconds = "0"+seconds;}
+    var time    = hours+':'+minutes+':'+seconds;
+    return time;
+}
+
+if(commandCheck("/uptime")){
+    var time = process.uptime();
+    var uptime = (time + "").toHHMMSS();
+    console.log(uptime);
+}
