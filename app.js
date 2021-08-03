b87bc9803fccfa92952872f326977e6104c3c426
itzJovens <82013409+itzJovens@users.noreply.github.com>
2021-08-03 17:08:18 -0400

    Update app.js

--- app.js
+++ app.js
@@ -27,83 +27,6 @@ client.on("USERNOTICE", onMessageHandler);
 // Connect to Twitch:
 client.connect();
 
-client.on("resub", function (channel, username, months, message, userstate, methods) {
-    // Do your stuff.
-  let cumulativeMonths = userstate['msg-param-cumulative-months'];
-// SRCHFPS
-  if (channel.includes('srchfps')){
-    client.say('srchfps', `PowerUpL Thanks for resubscribing @${username} for ${cumulativeMonths} MONTHS! PowerUpR`);
-    console.log(`* Executed Resub || SRCHFPS`)}
-});
-
-client.on("subscription", function (channel, username, method, message, userstate) {
-    // Do your stuff.
-// SRCHFPS
-  if (channel.includes('srchfps')){
-    client.say('srchfps', `PowerUpR Thanks for subscribing @${username} PowerUpR ||`);
-    console.log(`* Executed Sub || SRCHFPS`)}
-});
-
-client.on("subgift", (channel, username, streakMonths, recipient, methods, userstate) => {
-    // Do your stuff.
-  let senderCount = ~~userstate["msg-param-sender-count"];
-// SRCHFPS
-  if (channel.includes('srchfps')) {
-    client.say('srchfps', `PowerUpL GIFTED SUBS PowerUpR ||`);
-    let senderCount = ~~userstate["msg-param-sender-count"];
-    console.Log(`* Executed Gifted Subs || SRCHFPS`)}
-})
-
-// KARAGII
-client.on("subgift", (channel, username, streakMonths, recipient, methods, userstate) => {
-    // Do your stuff.
-  let senderCount = ~~userstate["msg-param-sender-count"];
-// SRCHFPS
-  if (channel.includes('karagii')) {
-	  syncDelay(3000)
-    client.say('karagii', `karagiMula GIFTED SUBS karagiMula`);
-    let senderCount = ~~userstate["msg-param-sender-count"];
-    console.Log(`* Executed Gifted Subs || KARAGII`)}
-})
-
-// SYDEON
-client.on("resub", function (channel, username, months, message, userstate, methods) {
-    // Do your stuff.
-  let cumulativeMonths = userstate['msg-param-cumulative-months'];
-	const { prime, plan, planName } = methods;
-// SRCHFPS
-  if (channel.includes('sydeon')){
-	  if (prime) {
-		  syncDelay(5000)
-    client.say('sydeon', `primer peepoPogStrip`);
-    console.log(`* Executed Resub || SYDEON`)}
-  }
-});
-
-client.on("subscription", function (channel, username, method, message, userstate) {
-    // Do your stuff.
-	const { prime, plan, planName } = method;
-// SRCHFPS
-  if (channel.includes('sydeon')){
-	  if (prime) {
-		  syncDelay(5000)
-    client.say('sydeon', `primer peepoPogStrip`);
-    console.log(`* Executed Sub || SYDEON`)}
-  }
-});
-
-client.on("subgift", (channel, username, streakMonths, recipient, methods, userstate) => {
-    // Do your stuff.
-  let senderCount = ~~userstate["msg-param-sender-count"];
-  if (channel.includes('sydeon')) {
-	  if (senderCount > 5) {
-	  syncDelay(5000)
-    client.say('uknwmyname', `PogU GIFTED SUBS sydeonHype`);
-    let senderCount = ~~userstate["msg-param-sender-count"];
-    console.Log(`* Executed Gifted Subs || SYDEON`)}
-  }
-})
-
 // TSM_DAEQUAN FOLLOWAGE 
 // FIRST PART
 client.on('message', (channel, tags, message, self) => {
@@ -202,11 +125,11 @@ client.on('message', (channel, tags, message, self) => {
 let isBroadcaster = channel.slice(1) === tags.username;
   if (isBroadcaster){
   if (channel.includes('itzjovens'))
-	if(message.toLowerCase().includes('|uptime')) {
+	if(message === '|uptime followage') {
     		var time = process.uptime();
     		var uptime = (time + "").toHHMMSS();
 	client.say('itzjovens', `${uptime}`);
-	console.log(`Executed uptime command in #itzjovens channel. || ${uptime} !`)
+	console.log(`Executed uptime command in #itzjovens channel for followage bot. || ${uptime} !`)
 	}
 	}
 });
