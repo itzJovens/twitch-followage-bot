9b167acf447be2bf93b9a6d4e71fe78fc01da9f6
itzJovens <82013409+itzJovens@users.noreply.github.com>
2021-08-04 00:00:40 -0400

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const opts = {
         password: 'oauth:u79zcmhtqum7o9we321k3xhpeh9x71'
     },
     channels: [
-        'itzjovens', 'uknwmyname', 'tsm_daequan', 'xdaboinextdoorx', 'karagii', 'sydeon'
+        'itzjovens', 'uknwmyname', 'tsm_daequan', 'xdaboinextdoorx',
     ]
 };
 
